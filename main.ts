c18928fc4bf70c6ccedef981ca54fe3f708c6cdb
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-03 16:31:52 -0500

    d

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan
+ * Created on: March 2026
+ * This program show the temperature in kelvin
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//variables
+let temperatureInCelsius: number
+let temperatureInKelvin: number
+
+input.onButtonPressed(Button.A, function () {
+    //get temperature (input)
+    temperatureInCelsius = input.temperature()
+    //process
+    temperatureInKelvin = (temperatureInCelsius + 273.15)
+    temperatureInKelvin = Math.round(temperatureInKelvin)
+
+    //output answer
+    basic.clearScreen()
+    basic.showString("The temperature is:" + temperatureInKelvin.toString() + 'K')
+})
